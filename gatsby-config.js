60041ba4c000b9cd7d712654d4cc66ed1fe7069c
Worker One <worker1@Loliver.localdomain>
2019-07-23 21:36:57 +1000

    add favicon and fix rendering of styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,10 @@ module.exports = {
     description: `Contact information and basic details.`,
     author: `@lolololiver`,
   },
+  pathPrefix: "/website",
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/contact-link/index.js
+++ src/components/contact-link/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-import { LinkWrapper } from  './style'
+import { LinkWrapper } from './style'
 
 const ContactLink = ({ color, href, children }) => (
-  <LinkWrapper color={color} href={href} target='_blank' rel='noopener noreferrer'>{children}</LinkWrapper>
+  <LinkWrapper
+    color={color}
+    href={href}
+    target='_blank'
+    rel='noopener noreferrer'
+  >
+    {children}
+  </LinkWrapper>
 )
 
 export default ContactLink
