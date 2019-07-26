aa51fe73c049cb56bc483f88869b86a020898715
Oliver Gassman <oll@olivergassman.net>
2019-07-26 16:42:34 +1000

    add words to link logos and a few more capabilities

--- src/components/contact-link/style.js
+++ src/components/contact-link/style.js
@@ -2,13 +2,22 @@ import styled from 'styled-components'
 
 export const LinkWrapper = styled.a`
   color: ${({ color }) => color};
+  padding: 0.25rem 0.75rem 0.25rem 0.25rem;
+  margin: 0.5rem;
+  border: 1px solid #eee;
+  border-radius: 0.25rem;
+  display: inline-block;
+  font-size: 1.5rem;
+  line-height: 3rem;
+  transform: translateY(0);
+  transition: transform 0.2s;
 
-  & svg {
-    transform: scale(1);
-    transition: transform 0.2s;
+  &:hover {
+    transform: translateY(-2px);
   }
+`
 
-  &:hover svg {
-    transform: scale(1.1);
-  }
+export const SvgLogoImg = styled.img`
+  margin: 0.5rem 0 -0.5rem 0.25rem;
+  width: 36px;
 `
